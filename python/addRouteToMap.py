09242c3c536dbc83b3769cbe0d010d9d0f8d5748
cssummer16 <cssummer16@c18.clinic.cs.hmc.edu>
2016-08-02 12:03:51 -0700

    update before adding projection on a line

--- python/addRouteToMap.py
+++ python/addRouteToMap.py
@@ -1,25 +1,33 @@
-import polyline
-import intersectionParse
+import polyline, math
+import intersectionParse, dirProject
 
 THRESHOLD = .0001101
 THRESHOLD = 0.0001
 
-#if a break was called
-# using so that I can tell when the path has finished if the inter.value > 0 
 # school = intersectionParse.Node([-117.71747, 34.10185,]) 
-# Currently dealing with the information given from google maps
 def addRoute (intersection, directionIntersection, points):
     mapOfNodes = {}
-    
+    close_intersections = []
     school = intersectionParse.Node(-117.71747, 34.10185)
-    #previous_node is only this value until
-    previous_node = intersectionParse.Node(-117.71747, 34.10185)
     previous_node = points[0]
 
     for point_node in points[1:]:
         if point_node in directionIntersection:
             directionIntersection.remove(point_node)
         #mapOfNodes[previous_node] = point_node
+        # print("Point_node, and Previous_node:", point_node.lat, previous_node.lat)
+        distance_lat = abs(point_node.lat - previous_node.lat)
+        distance_lng = abs(point_node.lng - previous_node.lng)
+        if  distance_lat >.00350 or distance_lng >.00350:
+            radius = (point_node - previous_node)/2
+            midpoint = intersectionParse.Node((min(point_node.lng, previous_node.lng) + distance_lng)/2, (min(point_node.lat, previous_node.lat) + distance_lat)/2)
+            close_intersections = [inter for inter in intersection if inter - midpoint <= radius]
+             
+            # Find intersections that are within the radius at the midpoint of two points
+            # Sort
+            # find the distance using projection of a line 
+        # print('here')
+
         mapOfNodes[point_node] = previous_node
         previous_node = point_node
     return mapOfNodes
@@ -36,4 +44,5 @@ def interExist(point, intersection):
     if best_distance < THRESHOLD:
         return best_node
     else:
-        return point
+        return point     
+
